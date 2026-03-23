3d5a2f57036e6d363d56838dd0e5888963704fac
Han Naung Soe <hannaungsoe2@gmailcom>
2026-03-23 16:12:35 +0630

    [mod] change japanese language

--- src/app/startup/startup.page.ts
+++ src/app/startup/startup.page.ts
@@ -83,8 +83,8 @@ export class StartupPage implements OnInit {
   async reload(): Promise<void> {
 
     await this.appInitService.reloadWebsite();
-    this.lastCheckedAt = new Date();
-  }
+    this.lastCheckedAt = new Date()
+  };
 
   async handleRefresh(event: RefresherCustomEvent): Promise<void> {
     try {
